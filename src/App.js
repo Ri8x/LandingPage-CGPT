e374735b1f124dbd2af2012d6d2e1f8aed5fe146
Mansoor Ahmed <42354272+Ri8x@users.noreply.github.com>
2022-12-27 05:23:39 +0000

    CGPT landing page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import './App.css';
-
-function App() {
+import React from 'react';
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="coming-soon">
+      <h1>Coming Soon</h1>
+      <p>We are working hard to bring you a new and improved experience. Please check back soon.</p>
     </div>
   );
 }
